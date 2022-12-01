e80d7543bcd98e9b0060debc3931df718bf75cee
PKornatowski <38524261+PKornatowski@users.noreply.github.com>
2022-12-01 19:57:13 +0100

    Startup

---
+++ D1/AoC2022 - D1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec  1 18:09:02 2022
+
+@author: Przemek
+"""
+
+input = list(open("input.txt","r"))
+
+sample = [1,2,3,4,5]
+print(sample[2])
+kalorie = 0
+#for i in input:
+#    kalorie = kalorie + 
