ee0f9c6dda27d8f527a39629ae6eb30efede34b5
PKornatowski <38524261+PKornatowski@users.noreply.github.com>
2022-12-01 21:08:03 +0100

    Update AoC2022 - D1.py

--- D1/AoC2022 - D1.py
+++ D1/AoC2022 - D1.py
@@ -5,10 +5,42 @@ Created on Thu Dec  1 18:09:02 2022
 @author: Przemek
 """
 
-input = list(open("input.txt","r"))
+sample = list(open("input.txt","r"))
 
-sample = [1,2,3,4,5]
-print(sample[2])
+#sample = ["1","2","3","6","\n","4","5", "\n","21", "100","\n"]
 kalorie = 0
+listakalori = []
+for item in sample:
+    if item != "\n":
+        #print(kalorie, "przed petla")
+        kalorie = kalorie + int(item)
+        #print(kalorie, "po petli")
+    else:
+        #print(kalorie, "suma pierwszej")
+        listakalori.append(kalorie)
+        #print(listakalori, "LISTA Z KALORIAMI")
+        kalorie = 0
+#print(index(max(listakalori)))
+
+listakalori.sort(reverse = True)
+suma = listakalori[0]+listakalori[1] + listakalori[2]
+print(suma)
+
+
+"""
+pierwszy=(max(listakalori))
+print(pierwszy, "1 wartosc")
+print(listakalori.index(pierwszy))
+delpierwszy = listakalori.index(pierwszy)
+del listakalori[delpierwszy]
+drugi=(max(listakalori))
+print(drugi, "2 wartosc")
+deldrugi = listakalori.index(drugi)
+del listakalori[deldrugi]
+print(max(listakalori), "3 wartosc")
+trzeci = max(listakalori)
+suma = pierwszy + drugi + trzeci
+print(suma)
+
 #for i in input:
-#    kalorie = kalorie + 
+"""
